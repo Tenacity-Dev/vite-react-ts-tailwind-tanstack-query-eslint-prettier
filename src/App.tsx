84c03df67e1d841b89460522ef32f822ca552a54
TenacityDev <98986694+Tenacity-Dev@users.noreply.github.com>
2024-03-16 12:40:41 +0100

    Add tanstack query

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
+import {useQuery} from "@tanstack/react-query";
+
 function App() {
+    const { data: todos } = useQuery({
+        queryKey: ['todos'],
+        queryFn: () => fetch('https://jsonplaceholder.typicode.com/todos').then((res) => res.json())
+    })
   return <div>
       <h1 className={'bg-red-500 font-bold text-3xl h-screen'}>Hello World</h1>
+      <pre>
+          {JSON.stringify(todos, null, 4)}
+      </pre>
   </div>
 }
 
