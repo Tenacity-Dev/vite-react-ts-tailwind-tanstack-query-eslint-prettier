082284a14e60b3050e53ba7c32b414eb12ff3f11
TenacityDev <98986694+Tenacity-Dev@users.noreply.github.com>
2024-03-16 12:44:44 +0100

    Add axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import {useQuery} from "@tanstack/react-query";
+import axios from "axios";
 
 function App() {
     const { data: todos } = useQuery({
         queryKey: ['todos'],
-        queryFn: () => fetch('https://jsonplaceholder.typicode.com/todos').then((res) => res.json())
+        queryFn: () => axios.get('https://jsonplaceholder.typicode.com/todos')
     })
+
   return <div>
       <h1 className={'bg-red-500 font-bold text-3xl h-screen'}>Hello World</h1>
       <pre>
